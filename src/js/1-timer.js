34c055904943ef5ffaf7d22558ac5c175ca81fef
r1shek <rostic523@gmail.com>
2024-04-30 20:35:37 +0200

    1

---
+++ src/js/1-timer.js
@@ -0,0 +1,19 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+let userSelectedDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+
+    console.log(selectedDates[0]);
+  },
+};
+flatpickr('#datetime-picker', options);
