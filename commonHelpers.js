b0dbe90001c80ea2bbbdf0c59c55b7282cff1859
r1shek <150455334+r1shek@users.noreply.github.com>
2024-04-30 18:35:54 +0000

    Deploying to gh-pages from @ r1shek/goit-js-hw-10@34c055904943ef5ffaf7d22558ac5c175ca81fef 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{f as t}from"./assets/vendor-2b44ac2e.js";const o={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){e[0],console.log(e[0])}};t("#datetime-picker",o);
+//# sourceMappingURL=commonHelpers.js.map
